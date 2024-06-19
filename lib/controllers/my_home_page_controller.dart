b5610cb08956a6d4a393703843feb13566b8f2e6
Brian Mongo <brian.mongo@tracom.co.ke>
2024-06-19 14:48:39 +0300

    internet checker and custom dialog connection

--- lib/controllers/my_home_page_controller.dart
+++ lib/controllers/my_home_page_controller.dart
@@ -1,112 +1,134 @@
 import 'dart:convert';
+import 'dart:isolate';
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:get/get.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:localstorage/localstorage.dart';
+import 'package:play_ground/widget/customDialog.dart';
 
-class MyHomePageController{
-  Map<String,dynamic> formInfo = <String,dynamic>{};
+class MyHomePageController {
+  Map<String, dynamic> formInfo = <String, dynamic>{};
   final LocalStorage storage = LocalStorage('my_app');
   late final mystorage;
   final formKey = GlobalKey<FormBuilderState>();
 
-  MyHomePageController(){
+  MyHomePageController() {
     initializeStorage();
     checkConnectionStatus();
+    constantConnectionStatus("false");
   }
-  checkConnectionStatus() async{
+  checkConnectionStatus() async {
     bool result = await InternetConnection().hasInternetAccess;
     print("checkConnectionStatus>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>--$result");
+    if (result == false) {
+      Get.dialog(CustomDialog(
+          actionMap: {
+            'Ok': ()=>print('ok')
+          },
+          title: 'Connection error',
+          description: "Check your internet connection and try again"));
+      constantConnectionStatus("false");
+    }
 
     return result;
+  }
+  constantConnectionStatus(cancel){
+    if (Get.isDialogOpen!=null && Get.isDialogOpen == true) {
+      constantConnectionStatus(true);
+    }
+    final listener = InternetConnection().onStatusChange.listen((InternetStatus status) {
+      switch (status) {
+        case InternetStatus.connected:
+        print('connected >>>>>>>>>>>>><<<<<<<<<<<<<<<< connected');
+        Get.dialog(CustomDialog(
+            actionMap: {
+              'Ok': () => print('ok'
+              )
+            },
+            title: 'Connection Success',
+            description: "internet connection available"));
+          break;
+        case InternetStatus.disconnected:
+          print('connected >>>>>>>>>>>>><<<<<<<<<<<<<<<NOT< connected');
 
-}
-  initializeStorage()async{
+          Get.dialog(CustomDialog(
+              actionMap: {
+                'Ok': () =>print('ok'
+                )
+              },
+              title: 'Connection error from checker',
+              description: "Check your internet connection and try again"));
+          break;
+      }
+    });
+    if(cancel==true){
+      listener.cancel();
+    }
+  }
+
+  initializeStorage() async {
     await storage.ready;
     mystorage = storage.getItem('myitems') ?? [];
   }
 
-  addItems(key,value)async{
-    var data =await storage.getItem(key);
-    var items =json.decode(data);
+  addItems(key, value) async {
+    var data = await storage.getItem(key);
+    var items = json.decode(data);
     items.add(value);
     var encodeddata = json.encode(items);
     storage.setItem(key, encodeddata);
   }
-  List<Map<String,dynamic>> items=[
-      {
-        'name': 'Smartphone',
-        'brand': 'Samsung',
-        'price': 799.99,
-        'color': 'Black'
-      },
-      {
-        'name': 'Laptop',
-        'brand': 'Dell',
-        'price': 1299.99,
-        'color': 'Silver'
-      },
-      {
-        'name': 'Headphones',
-        'brand': 'Sony',
-        'price': 149.99,
-        'color': 'Black'
-      },
-      {
-        'name': 'Tablet',
-        'brand': 'Apple',
-        'price': 599.99,
-        'color': 'Space Gray'
-      },
-      {
-        'name': 'Smartwatch',
-        'brand': 'Fitbit',
-        'price': 199.99,
-        'color': 'Blue'
-      },
-      {
-        'name': 'Camera',
-        'brand': 'Canon',
-        'price': 499.99,
-        'color': 'Black'
-      },
-      {
-        'name': 'Gaming Console',
-        'brand': 'Microsoft',
-        'price': 399.99,
-        'color': 'White'
-      },
-      {
-        'name': 'Wireless Speaker',
-        'brand': 'Bose',
-        'price': 299.99,
-        'color': 'Silver'
-      },
-      {
-        'name': 'Fitness Tracker',
-        'brand': 'Garmin',
-        'price': 149.99,
-        'color': 'Black'
-      },
-      {
-        'name': 'E-book Reader',
-        'brand': 'Amazon',
-        'price': 119.99,
-        'color': 'White'
-      },
-      {
-        'name': 'Drone',
-        'brand': 'DJI',
-        'price': 999.99,
-        'color': 'Gray'
-      },
-      {
-        'name': 'Smart Speaker',
-        'brand': 'Google',
-        'price': 149.99,
-        'color': 'Charcoal'
-      }
-    ];
 
+  List<Map<String, dynamic>> items = [
+    {
+      'name': 'Smartphone',
+      'brand': 'Samsung',
+      'price': 799.99,
+      'color': 'Black'
+    },
+    {'name': 'Laptop', 'brand': 'Dell', 'price': 1299.99, 'color': 'Silver'},
+    {'name': 'Headphones', 'brand': 'Sony', 'price': 149.99, 'color': 'Black'},
+    {
+      'name': 'Tablet',
+      'brand': 'Apple',
+      'price': 599.99,
+      'color': 'Space Gray'
+    },
+    {'name': 'Smartwatch', 'brand': 'Fitbit', 'price': 199.99, 'color': 'Blue'},
+    {'name': 'Camera', 'brand': 'Canon', 'price': 499.99, 'color': 'Black'},
+    {
+      'name': 'Gaming Console',
+      'brand': 'Microsoft',
+      'price': 399.99,
+      'color': 'White'
+    },
+    {
+      'name': 'Wireless Speaker',
+      'brand': 'Bose',
+      'price': 299.99,
+      'color': 'Silver'
+    },
+    {
+      'name': 'Fitness Tracker',
+      'brand': 'Garmin',
+      'price': 149.99,
+      'color': 'Black'
+    },
+    {
+      'name': 'E-book Reader',
+      'brand': 'Amazon',
+      'price': 119.99,
+      'color': 'White'
+    },
+    {'name': 'Drone', 'brand': 'DJI', 'price': 999.99, 'color': 'Gray'},
+    {
+      'name': 'Smart Speaker',
+      'brand': 'Google',
+      'price': 149.99,
+      'color': 'Charcoal'
+    }
+  ];
 }
